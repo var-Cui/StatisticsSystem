d1873ffd29c384fec4e49ba4107f8d6a3636ce0a
cuizm <cuizhimin@gloriousmed.com>
2020-03-04 15:32:25 +0800

    pom

--- src/main/java/com/statistics/demo/DemoApplication.java
+++ src/main/java/com/statistics/demo/DemoApplication.java
@@ -2,10 +2,16 @@ package com.statistics.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@RequestMapping("/")
+	public String index() {
+		return "hello";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
