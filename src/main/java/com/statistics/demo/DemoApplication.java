c4796ee16b0fd16317272f4a8dffc5559d91c61b
cuizm <cuizhimin@gloriousmed.com>
2020-03-05 15:23:39 +0800

    test

--- src/main/java/com/statistics/demo/DemoApplication.java
+++ src/main/java/com/statistics/demo/DemoApplication.java
@@ -2,20 +2,19 @@ package com.statistics.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@RestController
 @SpringBootApplication
-public class DemoApplication {
-
-	@RequestMapping("/")
-	public String index() {
-		return "hello";
-	}
+public class DemoApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
 	}
 
 }
--- src/main/java/com/statistics/demo/controller/firstController.java
+++ src/main/java/com/statistics/demo/controller/firstController.java
@@ -1,14 +1,14 @@
 package com.statistics.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class firstController {
 
-	@RequestMapping("/first")
+	@GetMapping("/first")
 	public String index() {
-		return "index";
+		return "welcome";
 	}
 	
 }
